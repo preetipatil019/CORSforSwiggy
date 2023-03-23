bb547732a8d2dee088d4908302056eca3defee9e
preeti <preetipatil019@gmail.com>
2023-03-23 14:37:35 -0700

    test case change

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/api/menu', (req, res) => {
 
 
 app.get('/', (req, res) =>{
-  res.json({"test":"hello instafood lovers !!! "});
+  res.json({"test":"hello Foodish !!! "});
 })
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
